b12c8620aa8f5d47bb8b5521678e99f14a3c90a7
Fedor-Fender <developerc26java@gmail.com>
2023-11-12 18:35:30 +0300

    Commit task_1

---
+++ src/com/techmeskills/homework_4/Task_1.java
@@ -0,0 +1,17 @@
+package com.techmeskills.homework_4;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        int[] array = {10, 15, 20, 25, 30};
+        System.out.println("Input your number of array");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i : array) {
+            if (i == number) {
+//               I don't know how to write code
+            }
+        }
+    }
+}

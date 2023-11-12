f20c7bf117e4e7bd0cb5c8cb33cffd79591fcd30
Fedor-Fender <developerc26java@gmail.com>
2023-11-12 17:48:57 +0300

    First commit
    this task was solved to determine whether number is in array or not

---
+++ src/com/techmeskills/homework_4/Task_0.java
@@ -0,0 +1,23 @@
+package com.techmeskills.homework_4;
+
+import java.util.Scanner;
+
+public class Task_0 {
+    public static void main(String[] args) {
+
+        int[] array = {10, 15, 20, 25, 30};
+        System.out.println("Input your number of array");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean found = true; // задал
+        for (int i : array) {
+            if (i == number) {
+                found = false;
+                System.out.println("This number is in array");
+                break;
+            }
+        }
+        if (found)
+            System.out.println("This number isn't in array");
+    }
+}
